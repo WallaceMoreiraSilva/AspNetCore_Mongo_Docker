b3bf31761d600cd42c6b64681011f3305d900b71
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-05-10 17:45:06 -0300

    Criando AspNetCore +Mongo

---
+++ Microservices/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,80 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existeProduto = productCollection.Find(p => true).Any();
+
+            if (!existeProduto)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        public static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "65400cb75d6f472897719b9f2cbfec9b",
+                    Name = "IPhone X",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Image = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "17f2ba934d214ff6860abceba7e6eb95",
+                    Name = "Samsung 10",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Image = "product-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "9c1a895de49f4484ad57dc12792e94cf",
+                    Name = "Huawei Plus",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Image = "product-3.png",
+                    Price = 650.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Id = "9c65739b9d9bf8450595c600ebe379703a",
+                    Name = "Xiaomi Mi 9",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Image = "product-4.png",
+                    Price = 470.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Id = "4acc4ed8e41a49dcb7e47e69a937e276",
+                    Name = "HTC U11+ Plus",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Image = "product-5.png",
+                    Price = 380.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "87aebcf7d3d84bb184825bbed60a317f",
+                    Name = "LG G7 ThinQ",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Image = "product-6.png",
+                    Price = 240.00M,
+                    Category = "Home Kitchen"
+                }
+            };
+        }
+    }
+}
