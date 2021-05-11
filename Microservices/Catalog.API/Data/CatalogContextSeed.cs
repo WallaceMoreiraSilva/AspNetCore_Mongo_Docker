2ec0617e6b82b3ab8adb0503800551fd6eecf825
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-05-11 15:41:19 -0300

    Ajuste na lista de produtos

--- Microservices/Catalog.API/Data/CatalogContextSeed.cs
+++ Microservices/Catalog.API/Data/CatalogContextSeed.cs
@@ -23,56 +23,56 @@ namespace Catalog.API.Data
                 new Product()
                 {
                     Id = "65400cb75d6f472897719b9f2cbfec9b",
-                    Name = "IPhone X",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "product-1.png",
-                    Price = 950.00M,
-                    Category = "Smart Phone"
+                    Name = "Caderno Espiral Pequeno",
+                    Description = "Caderno Espiral com 100 folhas pequeno.",
+                    Image = "caderno.png",
+                    Price = 7.65M,
+                    Category = "MaterialEscolar"
                 },
                 new Product()
                 {
                     Id = "17f2ba934d214ff6860abceba7e6eb95",
-                    Name = "Samsung 10",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "product-2.png",
-                    Price = 840.00M,
-                    Category = "Smart Phone"
+                    Name = "Borracha Branca Pequena",
+                    Description = "Borracha branca pequena para lápis.",
+                    Image = "borracha.png",
+                    Price = 4.55M,
+                    Category = "MaterialEscolar"
                 },
                 new Product()
                 {
                     Id = "9c1a895de49f4484ad57dc12792e94cf",
-                    Name = "Huawei Plus",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "product-3.png",
-                    Price = 650.00M,
-                    Category = "White Appliances"
+                    Name = "Estojo de Plástico Pequeno",
+                    Description = "Estojo de plástico pequeno azul.",
+                    Image = "estojo.png",
+                    Price = 6.79M,
+                    Category = "MaterialEscolar"
                 },
                 new Product()
                 {
                     Id = "9c65739b9d9bf8450595c600ebe379703a",
-                    Name = "Xiaomi Mi 9",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "product-4.png",
-                    Price = 470.00M,
-                    Category = "White Appliances"
+                    Name = "Clips para Pápeis Pequenos",
+                    Description = "Clips para pápeis pequenos 100g",
+                    Image = "clips.png",
+                    Price = 3.25M,
+                    Category = "MaterialEscolar"
                 },
                 new Product()
                 {
                     Id = "4acc4ed8e41a49dcb7e47e69a937e276",
-                    Name = "HTC U11+ Plus",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "product-5.png",
-                    Price = 380.00M,
-                    Category = "Smart Phone"
+                    Name = "Apontador Pequeno",
+                    Description = "Apontador pequeno preto",
+                    Image = "apontador.png",
+                    Price = 1.25M,
+                    Category = "MaterialEscolar"
                 },
                 new Product()
                 {
                     Id = "87aebcf7d3d84bb184825bbed60a317f",
-                    Name = "LG G7 ThinQ",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "product-6.png",
-                    Price = 240.00M,
-                    Category = "Home Kitchen"
+                    Name = "Papel A4",
+                    Description = "Papel A4 branco",
+                    Image = "papel.png",
+                    Price = 5.00M,
+                    Category = "MaterialEscolar"
                 }
             };
         }
